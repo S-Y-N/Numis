ba8114e41cecf91521f0196c705197befd2a4d64
Yevhen Trystan <kingguru@ukr.net>
2023-06-23 16:18:22 +0300

    add ui

--- src/main/java/com/example/numis/controllers/HomeController.java
+++ src/main/java/com/example/numis/controllers/HomeController.java
@@ -8,4 +8,21 @@ public class HomeController {
 
     @GetMapping("/home")
     public String getHome(){return "home/index";}
+    @GetMapping("/blog")
+    public String getBlog(){return "home/blog";}
+
+    @GetMapping("/catalog")
+    public String getCatalog(){return "home/catalog";}
+    @GetMapping("/mycollection")
+    public String getCollection(){return "home/collection";}
+    @GetMapping("/info")
+    public String getInfo(){return "home/info";}
+    @GetMapping("/profile")
+    public String getProfile(){return "home/profile";}
+    @GetMapping("/search")
+    public String getSearch(){return "home/search";}
+
+    @GetMapping("/enter")
+    public String getEnter(){return "user/login";}
+
 }
